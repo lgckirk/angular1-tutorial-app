b8927e048512f692a57aa00e69a2f841f5a68d08
gaochangli <lgckirk@gmail.com>
2018-05-19 23:09:26 -0500

    demonstrate the use of  service and ngSrc directive

--- modules/phone-list/phone-list.component.js
+++ modules/phone-list/phone-list.component.js
@@ -1,24 +1,26 @@
-// create (and register) phone-list component
-// note the 1 param module() fxn retrieves a module
+/**
+ * services are managed by the dependency injection system,
+ *    which keeps the app structured and loosely coupled
+ * the name of the service in the controller function signature is what DI uses
+ *    to do the injection, so it matters - use [name, fxn] in place of controller
+ *    function to solve the minification problem (parameter name got minified)
+ */
+
+// note $http is a XHR object
 angular.module("phoneList")
     .component("phoneList", {
         templateUrl: "./modules/phone-list/phone-list.template.html",
-        controller: function() {
-            this.phones = [
-              {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.',
-                age: 1
-              }, {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 2
-              }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 3
-              }
-            ];
+        controller: ["$http", function($http) {
+            // $http.get()/post() .. return a promise object
+            // we store "this" in a local variable so that even though by 
+            //    the time the callback runs, the context is undefined, 
+            //    we still have a means to reference the controller function obj
+            // Note the JSON is parsed automatically
+            var self = this;
+            $http.get("./resources/phones/phones.json").then(function(response){
+              self.phones = response.data;
+            });
+
             // NOTE: -'age' means orderBy age but in reverse order
             this.sortTerms = [
               {
@@ -34,7 +36,6 @@ angular.module("phoneList")
                 value: '-age'
               }
             ];
-            this.arr = [1,2];
             this.sortKey = this.sortTerms[1];
-        }
+        }]
     });
